558e51ea1e47570dd84af9a0a8660dc663b6ffbd
Ayush Kumar <115335623+Ayush-28-08@users.noreply.github.com>
2022-12-23 10:15:59 +0530

    Final

---
+++ hof/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HighOrderFunctions from './components/HighOrderFunctions';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <HighOrderFunctions />
+
+    </div>
+  );
+}
+
+export default App;
+
+
